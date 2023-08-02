e137fd051d54c422b630604232a2c39764e74b2b
ankitasharma1999 <98891495+ankitasharma1999@users.noreply.github.com>
2023-08-02 14:49:43 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+//import logo from './logo.svg';
+//import './App.css';
+// import Bodyy from "./project1/body";
+// import Header from "./project1/header";
+// import Car from "./project1/prop" ;
+//import Message from './project1/state1';
+//import Counter from "./project1/counter";
+// import FunctionComponent  from  "./project1/functioncounter";
+
+// import Hooks from "./project1/useeffect";
+
+import Bgcolor from "./project1/background";
+function App() {
+  return (
+    <div >
+      {/* <Header />
+      <Bodyy /> */}
+      {/* <Car /> */}
+      {/* <Message /> */}
+      {/* <Counter /> */}
+      {/* <functioncounter></functioncounter> */}
+      <Bgcolor />
+      {/* < Hooks /> */}
+    </div>
+  );
+}
+
+export default App;
